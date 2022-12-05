7591f68abd277412f1eba42eac23bff8f37892be
Alejandro <dantofema@gmail.com>
2022-12-05 19:10:03 -0300

    add - Superhero controller, model & migration

---
+++ database/migrations/2022_12_05_215957_create_superheroes_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('superheroes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('full_name')->nullable();
+            $table->string('strength')->nullable();
+            $table->string('speed')->nullable();
+            $table->string('durability')->nullable();
+            $table->string('power')->nullable();
+            $table->string('combat')->nullable();
+            $table->string('race')->nullable();
+            $table->string('height_0')->nullable();
+            $table->string('height_1')->nullable();
+            $table->string('weight_0')->nullable();
+            $table->string('weight_1')->nullable();
+            $table->string('eye_color')->nullable();
+            $table->string('hair_color')->nullable();
+            $table->string('publisher')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('superheroes');
+    }
+};
